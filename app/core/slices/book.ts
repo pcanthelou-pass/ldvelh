1918e21e221f279b2dc1ac22dab424e9eb9b7edb
pcanthelou-pass <119601402+pcanthelou-pass@users.noreply.github.com>
2025-05-20 18:50:51 +0200

    allow to add a full book

--- app/core/slices/book.ts
+++ app/core/slices/book.ts
@@ -5,10 +5,20 @@ export interface BookIntroduction {
   text: string
 }
 
+type EndPoint = 'end' | 'end-fail'
+
+export interface Scene {
+  question: string
+  text: string
+  next: Scenes | EndPoint
+}
+
+export type Scenes = Record<string, Scene>
 export interface Book {
   title: string
   description: string
-  introduction?: BookIntroduction
+  introduction: BookIntroduction
+  scenes: Record<string, Scene> | null
 }
 
 export const EmptyBook = {
@@ -18,11 +28,14 @@ export const EmptyBook = {
     title: '',
     text: '',
   },
+  scenes: null,
 }
 export interface BookActions {
+  setFullBook: (value: Book) => void
   setTitle: (value: string) => void
   setDescription: (value: string) => void
   setIntroduction: (value: BookIntroduction) => void
+  setScenes: (value: Record<string, Scene>) => void
 }
 
 export type BookSlice = Book & BookActions
@@ -31,8 +44,11 @@ export const createBookSlice: StateCreator<BookSlice, [], [], BookSlice> = (
   set,
 ) => ({
   ...EmptyBook,
+  setFullBook: (value: Book) => set((state) => ({ ...state, value })),
   setTitle: (value: string) => set((state) => ({ title: value })),
   setDescription: (value: string) => set((state) => ({ description: value })),
   setIntroduction: (value: BookIntroduction) =>
     set((state) => ({ introduction: value })),
+  setScenes: (value: Record<string, Scene>) =>
+    set((state) => ({ scenes: value })),
 })
