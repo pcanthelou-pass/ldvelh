4dbf60cf6415a12f541b76aa022623319518e96c
pcanthelou-pass <119601402+pcanthelou-pass@users.noreply.github.com>
2025-05-28 12:18:30 +0200

    refactor dices

--- app/core/classes/dice.ts
+++ app/core/classes/dice.ts
@@ -1,3 +1,17 @@
-export const D6 = (): number => 1 + Math.floor(Math.random() * 5)
+export class Dice {
+  private readonly side: number
 
-export const D6x2 = (): number => D6() + D6()
+  constructor(side: number = 6) {
+    this.side = side
+  }
+
+  roll(rolls: number = 1) {
+    return Array<number>(rolls)
+      .fill(1 + Math.floor(Math.random() * this.side))
+      .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+  }
+}
+
+export const D6 = (): number => new Dice().roll()
+
+export const D6x2 = (): number => new Dice().roll(2)
