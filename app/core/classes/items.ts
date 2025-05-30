37c721c72c48c77b4c2b19ea20293b4c8332087f
pcanthelou-pass <119601402+pcanthelou-pass@users.noreply.github.com>
2025-05-30 18:28:10 +0200

    colocate all items functions

--- app/core/classes/items.ts
+++ app/core/classes/items.ts
@@ -1,3 +1,5 @@
+import { GameActions } from '@core'
+
 export type ItemPowers = 'default' | 'potion'
 export interface ItemsProps {
   quantity: number
@@ -7,3 +9,64 @@ export interface ItemsProps {
 }
 
 export type Items = Record<string, ItemsProps>
+
+/*
+  items: {
+    "Potion d'endurance": {
+      quantity: 1,
+      value: 10,
+      power: 'potion',
+      effect: { endurance: 99 },
+    },
+  },
+*/
+
+export interface Item {
+  name: string
+  action: (store?: GameActions) => void
+}
+
+export type ItemBuilderProps = ItemsProps & { name: string }
+
+export const createItemTypeDefault = (item: ItemBuilderProps): Item => {
+  return {
+    name: item.name,
+    action: () => {},
+  }
+}
+export const createItemTypePotion = (item: ItemBuilderProps): Item => {
+  return {
+    name: `Boire la ${item.name.toLocaleLowerCase()}`,
+    action: (store: GameActions) => {
+      store.resetEndurance()
+      store.consumeItemByOne(item.name)
+    },
+  }
+}
+
+export type ItemFactory = (item: ItemBuilderProps) => Item
+
+export const ItemFactories: Record<string, ItemFactory> = {
+  default: createItemTypeDefault,
+  potion: createItemTypePotion,
+}
+
+export const ItemBuilder = (item: ItemBuilderProps): Item => {
+  let key: ItemPowers = item.power
+  if (!ItemFactories.hasOwnProperty(key)) {
+    key = 'default'
+  }
+  const factory: ItemFactory = ItemFactories[key]
+  return factory(item)
+}
+
+export const getActionableItems = (items: Items): Item[] => {
+  return Object.entries(items)
+    .filter(([key, item]) => item.quantity > 0)
+    .map(([key, item]) => {
+      return ItemBuilder({ ...item, name: key })
+    })
+}
+export const useItemSimpleParser = (items: Items) => {
+  return Object.keys(items)
+}
