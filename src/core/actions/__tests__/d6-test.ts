41a4f83c6980799bb435798b3e3542fdb003a737
pcanthelou-pass <119601402+pcanthelou-pass@users.noreply.github.com>
2025-07-13 17:25:57 +0200

    test: verify D6 boundaries with custom randomizers

--- src/core/actions/__tests__/d6-test.ts
+++ src/core/actions/__tests__/d6-test.ts
@@ -2,11 +2,12 @@ import { D6 } from '@actions'
 
 describe('Given a D6', () => {
   it('When launched it has a result between 1 and 6', () => {
-    const dice = D6(1, () => 1)
-    expect(dice).toBe(6)
+    expect(D6(1, () => 0)).toBe(1)
+    expect(D6(1, () => 0.9999)).toBe(6)
   })
+
   it('When launched D6x2 it has a result between 2 and 12', () => {
-    const dice = D6(2, () => 1)
-    expect(dice).toBe(12)
+    expect(D6(2, () => 0)).toBe(2)
+    expect(D6(2, () => 0.9999)).toBe(12)
   })
 })
